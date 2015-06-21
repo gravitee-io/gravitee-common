3c77d7b4f35a4332e88b23c43b7ddd2a9af52595
Azize Elamrani <azize.elamrani@gmail.com>
2015-06-21 16:41:23 +0200

    refactor: Initial commit of the Gravitee Common

---
+++ src/main/java/io/gravitee/common/component/AbstractLifecycleComponent.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.common.component;
+
+/**
+ * @author David BRASSELY (brasseld at gmail.com)
+ */
+public abstract class AbstractLifecycleComponent<T> implements LifecycleComponent<T> {
+
+  protected final Lifecycle lifecycle = new Lifecycle();
+
+  @Override
+  public Lifecycle.State lifecycleState() {
+    return this.lifecycle.state();
+  }
+
+  @Override
+  public T start() throws Exception {
+    doStart();
+    lifecycle.moveToStarted();
+
+    return (T) this;
+  }
+
+  @Override
+  public T stop() throws Exception {
+    lifecycle.moveToStopped();
+    doStop();
+
+    return (T) this;
+  }
+
+  protected abstract void doStart() throws Exception;
+
+  protected abstract void doStop() throws Exception;
+}

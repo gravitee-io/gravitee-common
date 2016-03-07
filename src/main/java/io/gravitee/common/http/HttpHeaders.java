fbd806c0f946ba70fcc63003c70b0a297f24be9d
Azize Elamrani <azize.elamrani@gmail.com>
2016-03-07 13:17:47 +0100

    feat: provides a list of media types and add some documentation on http headers list. This closes #1.

--- src/main/java/io/gravitee/common/http/HttpHeaders.java
+++ src/main/java/io/gravitee/common/http/HttpHeaders.java
@@ -28,77 +28,259 @@ import java.util.function.Function;
  */
 public class HttpHeaders implements MultiValueMap<String, String> {
 
-    public static final String CONNECTION = "Connection";
-    public static final String CACHE_CONTROL = "Cache-Control";
-    public static final String DATE = "Date";
-    public static final String PRAGMA = "Pragma";
-    public static final String PROXY_CONNECTION = "Proxy-Connection";
-    public static final String TRAILER = "Trailer";
-    public static final String TRANSFER_ENCODING = "Transfer-Encoding";
-    public static final String UPGRADE = "Upgrade";
-    public static final String VIA = "Via";
-    public static final String WARNING = "Warning";
-    public static final String NEGOTIATE = "Negotiate";
-    public static final String ALLOW = "Allow";
-    public static final String CONTENT_ENCODING = "Content-Encoding";
-    public static final String CONTENT_LANGUAGE = "Content-Language";
-    public static final String CONTENT_LENGTH = "Content-Length";
-    public static final String CONTENT_LOCATION = "Content-Location";
-    public static final String CONTENT_MD5 = "Content-MD5";
-    public static final String CONTENT_RANGE = "Content-Range";
-    public static final String CONTENT_TYPE = "Content-Type";
-    public static final String EXPIRES = "Expires";
-    public static final String LAST_MODIFIED = "Last-Modified";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.1">HTTP/1.1 documentation</a>}.
+     */
     public static final String ACCEPT = "Accept";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.2">HTTP/1.1 documentation</a>}.
+     */
     public static final String ACCEPT_CHARSET = "Accept-Charset";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.3">HTTP/1.1 documentation</a>}.
+     */
     public static final String ACCEPT_ENCODING = "Accept-Encoding";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.4">HTTP/1.1 documentation</a>}.
+     */
     public static final String ACCEPT_LANGUAGE = "Accept-Language";
-    public static final String AUTHORIZATION = "Authorization";
-    public static final String EXPECT = "Expect";
-    public static final String FORWARDED = "Forwarded";
-    public static final String FROM = "From";
-    public static final String HOST = "Host";
-    public static final String IF_MATCH = "If-Match";
-    public static final String IF_MODIFIED_SINCE = "If-Modified-Since";
-    public static final String IF_NONE_MATCH = "If-None-Match";
-    public static final String IF_RANGE = "If-Range";
-    public static final String IF_UNMODIFIED_SINCE = "If-Unmodified-Since";
-    public static final String KEEP_ALIVE = "Keep-Alive";
-    public static final String MAX_FORWARDS = "Max-Forwards";
-    public static final String PROXY_AUTHORIZATION = "Proxy-Authorization";
-    public static final String RANGE = "Range";
-    public static final String REQUEST_RANGE = "Request-Range";
-    public static final String REFERER = "Referer";
-    public static final String TE = "TE";
-    public static final String USER_AGENT = "User-Agent";
-    public static final String X_FORWARDED_FOR = "X-Forwarded-For";
-    public static final String X_FORWARDED_PROTO = "X-Forwarded-Proto";
-    public static final String X_FORWARDED_SERVER = "X-Forwarded-Server";
-    public static final String X_FORWARDED_HOST = "X-Forwarded-Host";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.5">HTTP/1.1 documentation</a>}.
+     */
     public static final String ACCEPT_RANGES = "Accept-Ranges";
-    public static final String AGE = "Age";
-    public static final String ETAG = "ETag";
-    public static final String LOCATION = "Location";
-    public static final String PROXY_AUTHENTICATE = "Proxy-Authenticate";
-    public static final String RETRY_AFTER = "Retry-After";
-    public static final String SERVER = "Server";
-    public static final String SERVLET_ENGINE = "Servlet-Engine";
-    public static final String VARY = "Vary";
-    public static final String WWW_AUTHENTICATE = "WWW-Authenticate";
-    public static final String COOKIE = "Cookie";
-    public static final String SET_COOKIE = "Set-Cookie";
-    public static final String SET_COOKIE2 = "Set-Cookie2";
-    public static final String MIME_VERSION = "MIME-Version";
-    public static final String IDENTITY = "identity";
-    public static final String X_POWERED_BY = "X-Powered-By";
+    /**
+     * See {@link <a href="https://www.w3.org/TR/cors/#access-control-allow-credentials-response-header">CORS documentation</a>}.
+     */
     public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    /**
+     * See {@link <a href="https://www.w3.org/TR/cors/#access-control-allow-headers-response-header">CORS documentation</a>}.
+     */
     public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    /**
+     * See {@link <a href="https://www.w3.org/TR/cors/#access-control-allow-methods-response-header">CORS documentation</a>}.
+     */
     public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    /**
+     * See {@link <a href="https://www.w3.org/TR/cors/#http-access-control-allow-origin">CORS documentation</a>}.
+     */
     public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    /**
+     * See {@link <a href="https://www.w3.org/TR/cors/#access-control-expose-headers-response-header">CORS documentation</a>}.
+     */
     public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+    /**
+     * See {@link <a href="https://www.w3.org/TR/cors/#access-control-max-age-response-header">CORS documentation</a>}.
+     */
     public static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+    /**
+     * See {@link <a href="https://www.w3.org/TR/cors/#access-control-request-headers-request-header">CORS documentation</a>}.
+     */
     public static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+    /**
+     * See {@link <a href="https://www.w3.org/TR/cors/#access-control-request-method-request-header">CORS documentation</a>}.
+     */
     public static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.6">HTTP/1.1 documentation</a>}.
+     */
+    public static final String AGE = "Age";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.7">HTTP/1.1 documentation</a>}.
+     */
+    public static final String ALLOW = "Allow";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.8">HTTP/1.1 documentation</a>}.
+     */
+    public static final String AUTHORIZATION = "Authorization";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.9">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CACHE_CONTROL = "Cache-Control";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.10">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CONNECTION = "Connection";
+    /**
+     * See {@link <a href="http://tools.ietf.org/html/rfc2183">IETF RFC-2183</a>}.
+     */
+    public static final String CONTENT_DISPOSITION = "Content-Disposition";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.11">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CONTENT_ENCODING = "Content-Encoding";
+    /**
+     * See {@link <a href="http://tools.ietf.org/html/rfc2392">IETF RFC-2392</a>}.
+     */
+    public static final String CONTENT_ID = "Content-ID";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.12">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CONTENT_LANGUAGE = "Content-Language";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.13">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CONTENT_LENGTH = "Content-Length";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.14">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CONTENT_LOCATION = "Content-Location";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.15">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CONTENT_MD5 = "Content-MD5";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.16">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CONTENT_RANGE = "Content-Range";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.17">HTTP/1.1 documentation</a>}.
+     */
+    public static final String CONTENT_TYPE = "Content-Type";
+    /**
+     * See {@link <a href="http://www.ietf.org/rfc/rfc2109.txt">IETF RFC 2109</a>}.
+     */
+    public static final String COOKIE = "Cookie";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.18">HTTP/1.1 documentation</a>}.
+     */
+    public static final String DATE = "Date";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.19">HTTP/1.1 documentation</a>}.
+     */
+    public static final String ETAG = "ETag";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.21">HTTP/1.1 documentation</a>}.
+     */
+    public static final String EXPIRES = "Expires";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.20">HTTP/1.1 documentation</a>}.
+     */
+    public static final String EXPECT = "Expect";
+    /**
+     * See {@link <a href="https://tools.ietf.org/html/rfc7239">IETF RFC 7239</a>}.
+     */
+    public static final String FORWARDED = "Forwarded";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.22">HTTP/1.1 documentation</a>}.
+     */
+    public static final String FROM = "From";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.23">HTTP/1.1 documentation</a>}.
+     */
+    public static final String HOST = "Host";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.24">HTTP/1.1 documentation</a>}.
+     */
+    public static final String IF_MATCH = "If-Match";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.25">HTTP/1.1 documentation</a>}.
+     */
+    public static final String IF_MODIFIED_SINCE = "If-Modified-Since";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.26">HTTP/1.1 documentation</a>}.
+     */
+    public static final String IF_NONE_MATCH = "If-None-Match";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.28">HTTP/1.1 documentation</a>}.
+     */
+    public static final String IF_UNMODIFIED_SINCE = "If-Unmodified-Since";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/HTTP/1.1/draft-ietf-http-v11-spec-01#Keep-Alive">HTTP/1.1 documentation</a>}.
+     */
+    public static final String KEEP_ALIVE = "Keep-Alive";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.29">HTTP/1.1 documentation</a>}.
+     */
+    public static final String LAST_MODIFIED = "Last-Modified";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.30">HTTP/1.1 documentation</a>}.
+     */
+    public static final String LOCATION = "Location";
+    /**
+     * See {@link <a href="http://tools.ietf.org/html/rfc5988#page-6">Web Linking (IETF RFC-5988) documentation</a>}.
+     */
+    public static final String LINK = "Link";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.31">HTTP/1.1 documentation</a>}.
+     */
+    public static final String MAX_FORWARDS = "Max-Forwards";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc1341/3_MIME-Version.html">HTTP/1.1 documentation</a>}.
+     */
+    public static final String MIME_VERSION = "MIME-Version";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.32">HTTP/1.1 documentation</a>}.
+     */
+    public static final String PRAGMA = "Pragma";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.33">HTTP/1.1 documentation</a>}.
+     */
+    public static final String PROXY_AUTHENTICATE = "Proxy-Authenticate";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.34">HTTP/1.1 documentation</a>}.
+     */
+    public static final String PROXY_AUTHORIZATION = "Proxy-Authorization";
+    public static final String PROXY_CONNECTION = "Proxy-Connection";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.35">HTTP/1.1 documentation</a>}.
+     */
+    public static final String RANGE = "Range";
+    /**
+     * See {@link <a href="https://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.36">HTTP/1.1 documentation</a>}.
+     */
+    public static final String REFERER = "Referer";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.37">HTTP/1.1 documentation</a>}.
+     */
+    public static final String RETRY_AFTER = "Retry-After";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.38">HTTP/1.1 documentation</a>}.
+     */
+    public static final String SERVER = "Server";
+    /**
+     * See {@link <a href="http://www.ietf.org/rfc/rfc2109.txt">IETF RFC 2109</a>}.
+     */
+    public static final String SET_COOKIE = "Set-Cookie";
+    /**
+     * See {@link <a href="https://www.ietf.org/rfc/rfc2965.txt">IETF RFC 2965</a>}.
+     */
+    public static final String SET_COOKIE2 = "Set-Cookie2";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.39">HTTP/1.1 documentation</a>}.
+     */
+    public static final String TE = "TE";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.40">HTTP/1.1 documentation</a>}.
+     */
+    public static final String TRAILER = "Trailer";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.41">HTTP/1.1 documentation</a>}.
+     */
+    public static final String TRANSFER_ENCODING = "Transfer-Encoding";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.42">HTTP/1.1 documentation</a>}.
+     */
+    public static final String UPGRADE = "Upgrade";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.43">HTTP/1.1 documentation</a>}.
+     */
+    public static final String USER_AGENT = "User-Agent";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.44">HTTP/1.1 documentation</a>}.
+     */
+    public static final String VARY = "Vary";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.45">HTTP/1.1 documentation</a>}.
+     */
+    public static final String VIA = "Via";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.46">HTTP/1.1 documentation</a>}.
+     */
+    public static final String WARNING = "Warning";
+    /**
+     * See {@link <a href="http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html#sec14.47">HTTP/1.1 documentation</a>}.
+     */
+    public static final String WWW_AUTHENTICATE = "WWW-Authenticate";
 
     private final Map<String, List<String>> headers;
 
---
+++ src/main/java/io/gravitee/common/http/MediaType.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.common.http;
+
+/**
+ * @author Azize Elamrani (azize dot elamrani at gmail dot com)
+ */
+public class MediaType {
+
+    /**
+     * The media type {@code charset} parameter name.
+     */
+    public static final String CHARSET_PARAMETER = "charset";
+    /**
+     * The value of a type or subtype wildcard {@value #MEDIA_TYPE_WILDCARD}.
+     */
+    public static final String MEDIA_TYPE_WILDCARD = "*";
+    /**
+     * A {@code String} constant representing wildcard {@value #WILDCARD} media type .
+     */
+    public final static String WILDCARD = "*/*";
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_XML} media type.
+     */
+    public final static String APPLICATION_XML = "application/xml";
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_ATOM_XML} media type.
+     */
+    public final static String APPLICATION_ATOM_XML = "application/atom+xml";
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_XHTML_XML} media type.
+     */
+    public final static String APPLICATION_XHTML_XML = "application/xhtml+xml";
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_SVG_XML} media type.
+     */
+    public final static String APPLICATION_SVG_XML = "application/svg+xml";
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_JSON} media type.
+     */
+    public final static String APPLICATION_JSON = "application/json";
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_FORM_URLENCODED} media type.
+     */
+    public final static String APPLICATION_FORM_URLENCODED = "application/x-www-form-urlencoded";
+    /**
+     * A {@code String} constant representing {@value #MULTIPART_FORM_DATA} media type.
+     */
+    public final static String MULTIPART_FORM_DATA = "multipart/form-data";
+    /**
+     * A {@code String} constant representing {@value #APPLICATION_OCTET_STREAM} media type.
+     */
+    public final static String APPLICATION_OCTET_STREAM = "application/octet-stream";
+    /**
+     * A {@code String} constant representing {@value #TEXT_PLAIN} media type.
+     */
+    public final static String TEXT_PLAIN = "text/plain";
+    /**
+     * A {@code String} constant representing {@value #TEXT_XML} media type.
+     */
+    public final static String TEXT_XML = "text/xml";
+    /**
+     * A {@code String} constant representing {@value #TEXT_HTML} media type.
+     */
+    public final static String TEXT_HTML = "text/html";
+}

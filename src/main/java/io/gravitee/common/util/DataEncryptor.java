5bbbc16a48447afc1eaf0b8418bb2674e37295be
Marc Cambier <marc.cambier@graviteesource.com>
2021-08-17 16:39:37 +0200

    feat: add DataEncryptor, which encrypt values in base64 using AES algorithm (code from cockpit's SecurityHandler, mutualized in commons to be used in APIM too)

---
+++ src/main/java/io/gravitee/common/util/DataEncryptor.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.common.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.env.Environment;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import java.util.Base64;
+
+import static javax.crypto.Cipher.DECRYPT_MODE;
+import static javax.crypto.Cipher.ENCRYPT_MODE;
+
+/**
+ * @author GraviteeSource Team
+ */
+public class DataEncryptor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DataEncryptor.class);
+    private static final String AES_ALGORITHM = "AES";
+
+    private String secret;
+
+    /**
+     * Builds a DataEncryptor, using a secret key from environment configuration.
+     *
+     * If secret key is not found in environment configuration,
+     * it uses a default key and displays a security warning.
+     *
+     * @param environment spring environment
+     * @param secretPropertyKey key of the property containing secret key in environment configuration
+     * @param defaultSecret default secret key to use
+     */
+    public DataEncryptor(Environment environment, String secretPropertyKey, String defaultSecret) {
+        this.secret = environment.getProperty(secretPropertyKey, defaultSecret);
+        if (defaultSecret.equals(secret)) {
+            LOGGER.warn("");
+            LOGGER.warn("##############################################################");
+            LOGGER.warn("#                      SECURITY WARNING                      #");
+            LOGGER.warn("##############################################################");
+            LOGGER.warn("");
+            LOGGER.warn("You still use the default secret.");
+            LOGGER.warn("This known secret can be used to access protected information.");
+            LOGGER.warn("Please customize the '{}' parameter value, or ask your administrator to do it.", secretPropertyKey);
+            LOGGER.warn("");
+            LOGGER.warn("##############################################################");
+            LOGGER.warn("");
+        }
+    }
+
+    /**
+     * Encrypt the specified value in a base64 representation.
+     *
+     * @param value the value to encrypt.
+     * @return the base64 representation of the encrypted value.
+     * @throws GeneralSecurityException thrown if an error occurred during encrypt.
+     */
+    public String encrypt(String value) throws GeneralSecurityException {
+        byte[] encrypted = buildCipher(ENCRYPT_MODE).doFinal(value.getBytes());
+        return Base64.getEncoder().encodeToString(encrypted);
+    }
+
+    /**
+     * Decrypt the specified base64 encrypted value.
+     *
+     * @param base64value the value to decrypt. Must be in base64.
+     * @return the decrypted value.
+     * @throws GeneralSecurityException thrown if an error occurred during decrypt.
+     */
+    public String decrypt(String base64value) throws GeneralSecurityException {
+        byte[] decrypted = buildCipher(DECRYPT_MODE).doFinal(Base64.getDecoder().decode(base64value));
+        return new String(decrypted);
+    }
+
+    private Cipher buildCipher(int cipherMode) throws GeneralSecurityException {
+        Cipher cipher = Cipher.getInstance(AES_ALGORITHM);
+        cipher.init(cipherMode, toSecretKeySpec(secret), cipher.getParameters());
+        return cipher;
+    }
+
+    private SecretKeySpec toSecretKeySpec(String secret) {
+        return new SecretKeySpec(secret.getBytes(), AES_ALGORITHM);
+    }
+}
---
+++ src/test/java/io/gravitee/common/util/DataEncryptorTest.java
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.common.util;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.core.env.Environment;
+
+import java.security.GeneralSecurityException;
+import java.security.InvalidKeyException;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class DataEncryptorTest {
+
+    private static final String SECRET_PROPERTY_KEY = "environment.property.key";
+    private static final String SECRET_KEY = "XxXxXZRmurH3YtzFCrYATkSG8H65xwah";
+    private static final String DEFAULT_SECRET_KEY = "XxXxXZ_DEFAULT_ONE_TkSG8H65xwah";
+    private static final String PLAIN_VALUE = "i want to encrypt and decrypt this string";
+    private static final String ENCRYPTED_VALUE = "AUkE3/wECYHLkkf+eh5PcKRETJdf+haNagK0bEk+R6DhPr2tm8/O7573fJ16IvFZ";
+
+    @Mock
+    private Environment environment;
+
+    @Test
+    public void should_encrypt_value() throws GeneralSecurityException {
+        mockEnvironmentContainingSecretKey();
+        String encryptedString = buildDataEncryptor().encrypt(PLAIN_VALUE);
+        assertEquals(ENCRYPTED_VALUE, encryptedString);
+    }
+
+    @Test
+    public void should_decrypt_value() throws GeneralSecurityException {
+        mockEnvironmentContainingSecretKey();
+        String decryptedString = buildDataEncryptor().decrypt(ENCRYPTED_VALUE);
+        assertEquals(PLAIN_VALUE, decryptedString);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void should_throw_exception_when_decrypting_invalid_encrypted_value() throws GeneralSecurityException {
+        mockEnvironmentContainingSecretKey();
+        buildDataEncryptor().decrypt("this is not encrypted");
+    }
+
+    @Test(expected = InvalidKeyException.class)
+    public void should_throw_exception_when_using_a_wrong_secret() throws GeneralSecurityException {
+        mockEnvironmentContainingWrongSecretKey();
+        buildDataEncryptor().encrypt(PLAIN_VALUE);
+    }
+
+    private DataEncryptor buildDataEncryptor() {
+        return new DataEncryptor(environment, SECRET_PROPERTY_KEY, DEFAULT_SECRET_KEY);
+    }
+
+    private void mockEnvironmentContainingSecretKey() {
+        when(environment.getProperty(SECRET_PROPERTY_KEY, DEFAULT_SECRET_KEY)).thenReturn(SECRET_KEY);
+    }
+
+    private void mockEnvironmentContainingWrongSecretKey() {
+        when(environment.getProperty(SECRET_PROPERTY_KEY, DEFAULT_SECRET_KEY)).thenReturn("wrongSecretKey");
+    }
+}

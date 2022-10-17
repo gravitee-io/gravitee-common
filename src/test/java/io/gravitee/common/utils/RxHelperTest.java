90b65d2730765cf57e3a1945fd3853f48bc4e4c4
Marc <marc.cambier@graviteesource.com>
2022-10-17 07:55:33 +0200

    chore: bump to rxJava3
    BREAKING CHANGE: rxJava3 required

--- src/test/java/io/gravitee/common/utils/RxHelperTest.java
+++ src/test/java/io/gravitee/common/utils/RxHelperTest.java
@@ -15,7 +15,7 @@
  */
 package io.gravitee.common.utils;
 
-import io.reactivex.Flowable;
+import io.reactivex.rxjava3.core.Flowable;
 import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.Test;
 

f2f935a5752043fc652891b955e72a2e473b74ac
Julien Giovaresco <dev@giovaresco.fr>
2023-08-07 15:43:32 +0200

    test: reorganize tests

--- src/test/java/io/gravitee/common/util/KeyStoreUtilsTest.java
+++ src/test/java/io/gravitee/common/util/KeyStoreUtilsTest.java
@@ -15,7 +15,12 @@
  */
 package io.gravitee.common.util;
 
-import static io.gravitee.common.util.KeyStoreUtils.*;
+import static io.gravitee.common.util.KeyStoreUtils.DEFAULT_ALIAS;
+import static io.gravitee.common.util.KeyStoreUtils.TYPE_JKS;
+import static io.gravitee.common.util.KeyStoreUtils.TYPE_PKCS12;
+import static io.gravitee.common.util.KeyStoreUtils.getCommonNamesByAlias;
+import static io.gravitee.common.util.KeyStoreUtils.merge;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.File;
 import java.io.IOException;
@@ -23,8 +28,12 @@ import java.nio.file.Files;
 import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.PrivateKey;
-import java.util.*;
-import org.junit.jupiter.api.Assertions;
+import java.util.Arrays;
+import java.util.Base64;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -33,114 +42,129 @@ import org.junit.jupiter.api.Test;
  */
 public class KeyStoreUtilsTest {
 
-    @Test
-    public void shouldInitFromPathPKCS12() throws IOException, KeyStoreException {
-        final KeyStore keyStore = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("all-in-one.p12"), "secret");
+    @Nested
+    class InitFromPath {
 
-        Assertions.assertNotNull(keyStore);
-        Assertions.assertEquals(4, keyStore.size());
+        @Test
+        public void should_init_pkcs12_keystore_from_path() throws KeyStoreException {
+            final KeyStore keyStore = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("all-in-one.p12"), "secret");
+
+            assertThat(keyStore.aliases().asIterator()).toIterable().hasSize(4);
+        }
+
+        @Test
+        public void should_init_jks_keystore_from_path() throws KeyStoreException {
+            final KeyStore keyStore = KeyStoreUtils.initFromPath(TYPE_JKS, getPath("all-in-one.jks"), "secret");
+
+            assertThat(keyStore.aliases().asIterator()).toIterable().hasSize(4);
+        }
     }
 
-    @Test
-    public void shouldInitFromContentPKCS12() throws IOException, KeyStoreException {
-        final KeyStore keyStore = KeyStoreUtils.initFromContent(TYPE_PKCS12, readContent("all-in-one.p12"), "secret");
+    @Nested
+    class InitFromContent {
 
-        Assertions.assertNotNull(keyStore);
-        Assertions.assertEquals(4, keyStore.size());
+        @Test
+        public void should_init_pkcs12_from_content() throws IOException, KeyStoreException {
+            final KeyStore keyStore = KeyStoreUtils.initFromContent(TYPE_PKCS12, readContent("all-in-one.p12"), "secret");
+
+            assertThat(keyStore.aliases().asIterator()).toIterable().hasSize(4);
+        }
+
+        @Test
+        public void should_init_jks_from_content() throws IOException, KeyStoreException {
+            final KeyStore keyStore = KeyStoreUtils.initFromContent(TYPE_JKS, readContent("all-in-one.jks"), "secret");
+
+            assertThat(keyStore.aliases().asIterator()).toIterable().hasSize(4);
+        }
     }
 
-    @Test
-    public void shouldInitFromPathJKS() throws IOException, KeyStoreException {
-        final KeyStore keyStore = KeyStoreUtils.initFromPath(TYPE_JKS, getPath("all-in-one.jks"), "secret");
+    @Nested
+    class LoadPemPrivateKey {
 
-        Assertions.assertNotNull(keyStore);
-        Assertions.assertEquals(4, keyStore.size());
+        @Test
+        public void should_load_pkcs8_key() throws IOException {
+            final PrivateKey privateKey = KeyStoreUtils.loadPemPrivateKey(readContent("rsakey.pem", false));
+
+            assertThat(privateKey).isNotNull();
+        }
     }
 
-    @Test
-    public void shouldInitFromContentJKS() throws IOException, KeyStoreException {
-        final KeyStore keyStore = KeyStoreUtils.initFromContent(TYPE_JKS, readContent("all-in-one.jks"), "secret");
+    @Nested
+    class InitFromPem {
 
-        Assertions.assertNotNull(keyStore);
-        Assertions.assertEquals(4, keyStore.size());
+        @Test
+        public void should_init_from_certificate_and_key_content() throws IOException, KeyStoreException {
+            final String certPem = readContent("localhost.cer", false);
+            final String pkPem = readContent("localhost.key", false);
+            final KeyStore keyStore = KeyStoreUtils.initFromPem(certPem, pkPem, "secret", null);
+
+            assertThat(keyStore.aliases().asIterator()).toIterable().hasSize(1).contains(DEFAULT_ALIAS);
+        }
+
+        @Test
+        public void should_init_from_certificate_and_key_content_with_custom_alias() throws IOException, KeyStoreException {
+            final String certPem = readContent("localhost.cer", false);
+            final String pkPem = readContent("localhost.key", false);
+            final KeyStore keyStore = KeyStoreUtils.initFromPem(certPem, pkPem, "secret", "custom-alias");
+
+            assertThat(keyStore.aliases().asIterator()).toIterable().hasSize(1).contains("custom-alias");
+        }
+
+        @Test
+        public void should_init_from_certificate_and_key_paths() throws KeyStoreException {
+            List<String> certs = List.of(
+                getPath("localhost.cer"),
+                getPath("localhost2.cer"),
+                getPath("localhost3.cer"),
+                getPath("wildcard.cer")
+            );
+
+            List<String> keys = List.of(
+                getPath("localhost.key"),
+                getPath("localhost2.key"),
+                getPath("localhost3.key"),
+                getPath("wildcard.key")
+            );
+
+            final KeyStore keyStore = KeyStoreUtils.initFromPems(certs, keys, "secret");
+
+            assertThat(keyStore.aliases().asIterator()).toIterable().hasSize(4);
+        }
     }
 
-    @Test
-    public void shouldLoadPKCS8Key() throws IOException, KeyStoreException {
-        final PrivateKey privateKey = KeyStoreUtils.loadPemPrivateKey(readContent("rsakey.pem", false));
+    @Nested
+    class Merge {
 
-        Assertions.assertNotNull(privateKey);
+        @Test
+        public void should_merge_key_stores() throws KeyStoreException {
+            final KeyStore source1 = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("localhost.p12"), "secret");
+            final KeyStore source2 = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("localhost2.p12"), "secret");
+            final KeyStore source3 = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("localhost3.p12"), "secret");
+            final KeyStore source4 = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("wildcard.p12"), "secret");
+
+            final KeyStore keyStore = merge(Arrays.asList(source1, source2, source3, source4), "secret");
+
+            assertThat(keyStore.aliases().asIterator()).toIterable().hasSize(4);
+        }
     }
 
-    @Test
-    public void shouldInitFromContentPem() throws IOException, KeyStoreException {
-        final String certPem = readContent("localhost.cer", false);
-        final String pkPem = readContent("localhost.key", false);
-        final KeyStore keyStore = KeyStoreUtils.initFromPem(certPem, pkPem, "secret", null);
+    @Nested
+    class GetCommonNamesByAlias {
 
-        Assertions.assertNotNull(keyStore);
-        Assertions.assertEquals(1, keyStore.size());
-        Assertions.assertEquals(DEFAULT_ALIAS, keyStore.aliases().nextElement());
-    }
+        @Test
+        public void should_get_cn_aliases_with_SAN_and_wildcard() {
+            final KeyStore keyStore = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("all-in-one.p12"), "secret");
 
-    @Test
-    public void shouldInitFromContentPemWithCustomAlias() throws IOException, KeyStoreException {
-        final String certPem = readContent("localhost.cer", false);
-        final String pkPem = readContent("localhost.key", false);
-        final KeyStore keyStore = KeyStoreUtils.initFromPem(certPem, pkPem, "secret", "custom-alias");
+            final Map<String, String> commonNamesByAlias = getCommonNamesByAlias(keyStore);
 
-        Assertions.assertNotNull(keyStore);
-        Assertions.assertEquals(1, keyStore.size());
-        Assertions.assertEquals("custom-alias", keyStore.aliases().nextElement());
-    }
-
-    @Test
-    public void shouldInitFromContentPathPems() throws IOException, KeyStoreException {
-        List<String> certs = new ArrayList<>();
-        List<String> keys = new ArrayList<>();
-
-        certs.add(getPath("localhost.cer"));
-        certs.add(getPath("localhost2.cer"));
-        certs.add(getPath("localhost3.cer"));
-        certs.add(getPath("wildcard.cer"));
-
-        keys.add(getPath("localhost.key"));
-        keys.add(getPath("localhost2.key"));
-        keys.add(getPath("localhost3.key"));
-        keys.add(getPath("wildcard.key"));
-
-        final KeyStore keyStore = KeyStoreUtils.initFromPems(certs, keys, "secret");
-
-        Assertions.assertNotNull(keyStore);
-        Assertions.assertEquals(4, keyStore.size());
-    }
-
-    @Test
-    public void shouldMergeKeyStores() throws KeyStoreException {
-        final KeyStore source1 = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("localhost.p12"), "secret");
-        final KeyStore source2 = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("localhost2.p12"), "secret");
-        final KeyStore source3 = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("localhost3.p12"), "secret");
-        final KeyStore source4 = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("wildcard.p12"), "secret");
-
-        final KeyStore keyStore = merge(Arrays.asList(source1, source2, source3, source4), "secret");
-
-        Assertions.assertNotNull(keyStore);
-        Assertions.assertEquals(4, keyStore.size());
-    }
-
-    @Test
-    public void shouldGetCNAliasesWithSANAndWildcard() {
-        final KeyStore keyStore = KeyStoreUtils.initFromPath(TYPE_PKCS12, getPath("all-in-one.p12"), "secret");
-
-        final Map<String, String> commonNamesByAlias = getCommonNamesByAlias(keyStore);
-
-        Assertions.assertNotNull(commonNamesByAlias);
-        Assertions.assertEquals(5, commonNamesByAlias.size());
-        Assertions.assertEquals("localhost", commonNamesByAlias.get("localhost"));
-        Assertions.assertEquals("localhost2", commonNamesByAlias.get("localhost2"));
-        Assertions.assertEquals("localhost3", commonNamesByAlias.get("localhost3"));
-        Assertions.assertEquals("localhost3", commonNamesByAlias.get("xyz.localhost.com"));
-        Assertions.assertEquals("wildcard", commonNamesByAlias.get("*.localhost.com"));
+            assertThat(commonNamesByAlias)
+                .hasSize(5)
+                .containsEntry("localhost", "localhost")
+                .containsEntry("localhost2", "localhost2")
+                .containsEntry("localhost3", "localhost3")
+                .containsEntry("xyz.localhost.com", "localhost3")
+                .containsEntry("*.localhost.com", "wildcard");
+        }
     }
 
     private String readContent(String resource) throws IOException {
@@ -155,6 +179,6 @@ public class KeyStoreUtilsTest {
     }
 
     private String getPath(String resource) {
-        return this.getClass().getResource("/keystores/" + resource).getPath();
+        return Objects.requireNonNull(this.getClass().getResource("/keystores/" + resource)).getPath();
     }
 }

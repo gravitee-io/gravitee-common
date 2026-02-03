954fabc96ffe2d7068ff4e0febc767676abb1e11
Benoit Bordigoni <benoit.bordigoni@graviteesource.com>
2026-02-03 17:40:18 +0100

    feat: add PKCS7 utility class

---
+++ src/test/java/io/gravitee/common/security/PKCS7UtilsTest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright © 2015 The Gravitee team (http://gravitee.io)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.gravitee.common.security;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import io.gravitee.common.util.KeyStoreUtils;
+import java.security.KeyStore;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateException;
+import java.util.Enumeration;
+import java.util.List;
+import java.util.Optional;
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author Benoit BORDIGONI (benoit.bordigoni at graviteesource.com)
+ * @author GraviteeSource Team
+ */
+@DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
+class PKCS7UtilsTest {
+
+    private static final String PEM_CERTIFICATE_1 =
+        """
+            -----BEGIN CERTIFICATE-----
+            MIIDCTCCAfGgAwIBAgIUdh1NFpteTomLlWoO7O5HKI7fg10wDQYJKoZIhvcNAQEL
+            BQAwFDESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTI2MDEyOTE0MTYyMVoXDTI3MDEy
+            OTE0MTYyMVowFDESMBAGA1UEAwwJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0BAQEF
+            AAOCAQ8AMIIBCgKCAQEAwquFLM7wi+EBt5JL1Q6c/qzqickBKhlymOf1a18jYsWO
+            UrRRqANMvEub5zks44qdYbdm7Kj9EruDD3hfrS6YemQhIMeT+SgY3MU5cY12yB1e
+            fn+0bkEg6CJdIfgvcuccqY9pu0hgFdlgK9YXEXYZzb+ai7b4qPHN2BR6toBdjf56
+            ReNygcrT0igPQC9P/MsmpFuJzD69i5Z8fJcLU2V7RwXW9MKyej8CN/zrcqftG+ck
+            egt5cpB2OyIt6ajQBeXarGYvOlm975RKWOD5mHpt87GgcEvMEnhFTSs47iA7X91o
+            01+rhFDfPxgvhur2AG6oiB7/zS4lqHWQHRTBwurVJQIDAQABo1MwUTAdBgNVHQ4E
+            FgQUHP3c1CNhl6RZHn3g/HkbSssfPhMwHwYDVR0jBBgwFoAUHP3c1CNhl6RZHn3g
+            /HkbSssfPhMwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAgTa/
+            lgY6fEbt88yQcLxQDseam2lox0sK6sYOwpIQV7/RiXbeM6KrXmCy59HBrJMjSpZY
+            LEp9RPDf8Awg50iv6oFXXn+ZJ5Cmq2WXMpCvKxAjQWnmNs99SGfXyQsiLMxe3HlL
+            CKqM8O7LZrVdOxWbNW/0ZMJl4d4vCf0LhVrbfMGLeQfqtKVmygjJM1rycKiFazM4
+            cTHphvWA9/5XRFC+yD3V3ZTFE9LDeMoSF0soigR0NnCqFc5E7S9OQvuB5h5eBu9s
+            T1dLBVOY8zcIYu6LDjeMr6MpiZyk+/O7ewue2kQURmDBuVrwiSSQF4AhsmbMDUuB
+            iyr26LfMYtituDZy7w==
+            -----END CERTIFICATE-----
+            """;
+
+    private static final String PEM_CERTIFICATE_2 =
+        """
+            -----BEGIN CERTIFICATE-----
+            MIIDCTCCAfGgAwIBAgIUYh2NFpteTomLlWoO7O5HKI7fg10wDQYJKoZIhvcNAQEL
+            BQAwFDESMBAGA1UEAwwJbG9jYWxob3N0MB4XDTI2MDEyOTE0MTYyMVoXDTI3MDEy
+            OTE0MTYyMVowFDESMBAGA1UEAwwJbG9jYWxob3N0MIIBIjANBgkqhkiG9w0BAQEF
+            AAOCAQ8AMIIBCgKCAQEAwquFLM7wi+EBt5JL1Q6c/qzqickBKhlymOf1a18jYsWO
+            UrRRqANMvEub5zks44qdYbdm7Kj9EruDD3hfrS6YemQhIMeT+SgY3MU5cY12yB1e
+            fn+0bkEg6CJdIfgvcuccqY9pu0hgFdlgK9YXEXYZzb+ai7b4qPHN2BR6toBdjf56
+            ReNygcrT0igPQC9P/MsmpFuJzD69i5Z8fJcLU2V7RwXW9MKyej8CN/zrcqftG+ck
+            egt5cpB2OyIt6ajQBeXarGYvOlm975RKWOD5mHpt87GgcEvMEnhFTSs47iA7X91o
+            01+rhFDfPxgvhur2AG6oiB7/zS4lqHWQHRTBwurVJQIDAQABo1MwUTAdBgNVHQ4E
+            FgQUHP3c1CNhl6RZHn3g/HkbSssfPhMwHwYDVR0jBBgwFoAUHP3c1CNhl6RZHn3g
+            /HkbSssfPhMwDwYDVR0TAQH/BAUwAwEB/zANBgkqhkiG9w0BAQsFAAOCAQEAgTa/
+            lgY6fEbt88yQcLxQDseam2lox0sK6sYOwpIQV7/RiXbeM6KrXmCy59HBrJMjSpZY
+            LEp9RPDf8Awg50iv6oFXXn+ZJ5Cmq2WXMpCvKxAjQWnmNs99SGfXyQsiLMxe3HlL
+            CKqM8O7LZrVdOxWbNW/0ZMJl4d4vCf0LhVrbfMGLeQfqtKVmygjJM1rycKiFazM4
+            cTHphvWA9/5XRFC+yD3V3ZTFE9LDeMoSF0soigR0NnCqFc5E7S9OQvuB5h5eBu9s
+            T1dLBVOY8zcIYu6LDjeMr6MpiZyk+/O7ewue2kQURmDBuVrwiSSQF4AhsmbMDUuB
+            iyr26LfMYtituDZy7w==
+            -----END CERTIFICATE-----
+            """;
+
+    private static final String WRONG_PEM_CERTIFICATE_1 =
+        """
+            -----BEGIN CERTIFICATE-----
+            SHITE
+            -----END CERTIFICATE-----
+            """;
+
+    @Test
+    void should_create_and_read_bundle_from_right_certificate() throws CertificateException, KeyStoreException, NoSuchAlgorithmException {
+        List<String> certs = List.of(PEM_CERTIFICATE_1, PEM_CERTIFICATE_2);
+        byte[] bundle = PKCS7Utils.createBundle(certs);
+        assertThat(bundle).isNotEmpty();
+
+        Optional<KeyStore> keyStore = PKCS7Utils.pkcs7ToTruststore(bundle, null, String::valueOf, false);
+        assertThat(keyStore)
+            .isPresent()
+            .get()
+            .satisfies(ks -> {
+                Enumeration<String> aliases = ks.aliases();
+                assertThat(aliases.hasMoreElements()).isTrue();
+                assertThat(aliases.nextElement()).isEqualTo("0");
+                assertThat(aliases.nextElement()).isEqualTo("1");
+                assertThat(aliases.hasMoreElements()).isFalse();
+
+                assertThat(ks.getCertificate("0")).isEqualTo(KeyStoreUtils.loadPemCertificates(PEM_CERTIFICATE_1)[0]);
+                assertThat(ks.getCertificate("1")).isEqualTo(KeyStoreUtils.loadPemCertificates(PEM_CERTIFICATE_2)[0]);
+            });
+    }
+
+    @Test
+    void should_create_a_bundle_from_no_certs() {
+        assertThatCode(() -> PKCS7Utils.createBundle(List.of())).doesNotThrowAnyException();
+    }
+
+    @Test
+    void should_create_and_read_empty_bundle() {
+        byte[] bundle = PKCS7Utils.createBundle(List.of());
+        assertThat(bundle).isNotEmpty();
+        assertThatCode(() -> PKCS7Utils.pkcs7ToTruststore(bundle, null, String::valueOf, true)).doesNotThrowAnyException();
+        assertThatCode(() -> PKCS7Utils.pkcs7ToTruststore(bundle, null, String::valueOf, false))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void should_not_create_bundle() {
+        List<String> wrongPemCertificate = List.of(WRONG_PEM_CERTIFICATE_1);
+        assertThatCode(() -> PKCS7Utils.createBundle(wrongPemCertificate)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void should_read_empty_bundle() throws CertificateException, KeyStoreException, NoSuchAlgorithmException {
+        Optional<KeyStore> keyStore = PKCS7Utils.pkcs7ToTruststore(new byte[0], null, String::valueOf, true);
+        assertThat(keyStore).isEmpty();
+    }
+}
